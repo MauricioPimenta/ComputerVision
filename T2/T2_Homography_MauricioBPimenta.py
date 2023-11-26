abc833b57a425239c5e0445503df448170feb76d
MauricioPimenta <mauriciobpimenta@gmail.com>
2023-11-26 04:56:18 -0300

    starting over

--- T2/T2_Homography_MauricioBPimenta.py
+++ T2/T2_Homography_MauricioBPimenta.py
@@ -18,30 +18,65 @@ def normalize_points(points):
 
 
 
-    return norm_points, T
+	return norm_points, T
 
 # Função para montar a matriz A do sistema de equações do DLT
 # Entrada: pts1, pts2 (pontos "pts1" da primeira imagem e pontos "pts2" da segunda imagem que atendem a pts2=H.pts1)
 # Saída: A (matriz com as duas ou três linhas resultantes da relação pts2 x H.pts1 = 0)
 def compute_A(pts1, pts2):
 
-    return A
+	# Compute matrix A
+	# Inicializa a Matriz A com zeros -> Tamanho: 2*(qtd de pontos (x,y) de pts1) de altura e 9 de largura (colunas)
+	A = np.zeros((2*pts1.shape[1], 9))
+
+	# Para um ponto, a matriz A é calculada como:
+	#
+	# |     0         0         0       -wi2*xi1   -wi2*yi1   -wi2*wi1   yi2*xi1    yi2*yi1    yi2*wi1   |
+	# |  wi2*xi1   wi2*yi1   wi2*wi1        0          0          0     -xi2*xi1   -xi2*yi1   -xi2*wi1   |
+	#
+	for i in range(pts1.shape[1]):
+
+		# Calcula a primeira linha da matriz A pra todos os pontos
+		A[2*i, 3:6] = -pts2[2, i]*pts1[:,i]     # A[3:6] = -wi2*Xi.T
+		A[2*i, 6:9] = +pts2[1, i]*pts1[:,i]     # A[6:9] = +yi2*Xi.T
+
+		# Calcula a segunda linha da matriz A pra todos os pontos
+		A[2*i+1, 0:3] = +pts2[2, i]*pts1[:,i]   # A[0:3] = +wi2*Xi.T
+		A[2*i+1, 6:9] = -pts2[0, i]*pts1[:,i]   # A[6:9] = -xi2*Xi.T
+
+
+	return A
 
 # Função do DLT Normalizado
 # Entrada: pts1, pts2 (pontos "pts1" da primeira imagem e pontos "pts2" da segunda imagem que atendem a pts2=H.pts1)
 # Saída: H (matriz de homografia estimada)
 def compute_normalized_dlt(pts1, pts2):
 
-    # Normaliza pontos
+	# # Add homogeneous coordinates
+	# pts1 = np.transpose(pts1)
+	# pts1 = np.vstack( (pts1, np.ones(np.shape(pts1[0]))) )
 
-    # Constrói o sistema de equações empilhando a matrix A de cada par de pontos correspondentes normalizados
+	# pts2 = np.transpose(pts2)
+	# pts2 = np.vstack( (pts2, np.ones(np.shape(pts2[0]))) )
 
-    # Calcula o SVD da matriz A_empilhada e estima a homografia H_normalizada
+	# Normaliza pontos
+	pts1_norm = normalize_points(pts1)
+	pts2_norm = normalize_points(pts2)
 
-    # Denormaliza H_normalizada e obtém H
+	# Constrói o sistema de equações empilhando a matrix A de cada par de pontos correspondentes normalizados
+	A = compute_A(pts1_norm, pts2_norm)
 
+	# Calcula o SVD da matriz A_empilhada e estima a homografia H_normalizada
 
-    return H
+	# Perform SVD(A) = U.S.Vt to estimate the homography
+	v = np.linalg.svd(A)    # return all three arrays into 'v'
+
+	# Takes the last column of the last array of 'v' and reshapes it as the homography matrix
+	H_matrix = np.reshape(v[-1][-1], (3,3)); print('\nH_Matrix: \n', H_matrix, '\n')
+
+	# Denormaliza H_normalizada e obtém H
+
+	return H_matrix
 
 
 # Função do RANSAC
@@ -59,26 +94,26 @@ def compute_normalized_dlt(pts1, pts2):
 
 def RANSAC(pts1, pts2, dis_threshold, N, Ninl):
 
-    # Define outros parâmetros como número de amostras do modelo, probabilidades da equação de N, etc
+	# Define outros parâmetros como número de amostras do modelo, probabilidades da equação de N, etc
 
 
-    # Processo Iterativo
-        # Enquanto não atende a critério de parada
+	# Processo Iterativo
+		# Enquanto não atende a critério de parada
 
-        # Sorteia aleatoriamente "s" amostras do conjunto de pares de pontos pts1 e pts2
+		# Sorteia aleatoriamente "s" amostras do conjunto de pares de pontos pts1 e pts2
 
-        # Usa as amostras para estimar uma homografia usando o DTL Normalizado
+		# Usa as amostras para estimar uma homografia usando o DTL Normalizado
 
-        # Testa essa homografia com os demais pares de pontos usando o dis_threshold e contabiliza
-        # o número de supostos inliers obtidos com o modelo estimado
+		# Testa essa homografia com os demais pares de pontos usando o dis_threshold e contabiliza
+		# o número de supostos inliers obtidos com o modelo estimado
 
-        # Se o número de inliers é o maior obtido até o momento, guarda esse conjunto além das "s" amostras utilizadas.
-        # Atualiza também o número N de iterações necessárias
+		# Se o número de inliers é o maior obtido até o momento, guarda esse conjunto além das "s" amostras utilizadas.
+		# Atualiza também o número N de iterações necessárias
 
-    # Terminado o processo iterativo
-    # Estima a homografia final H usando todos os inliers selecionados.
+	# Terminado o processo iterativo
+	# Estima a homografia final H usando todos os inliers selecionados.
 
-    return H, pts1_in, pts2_in
+	return H, pts1_in, pts2_in
 
 
 ########################################################################################################################
@@ -105,26 +140,26 @@ matches = flann.knnMatch(des1, des2, k=2)
 
 good = []
 for m, n in matches:
-    if m.distance < 0.75 * n.distance:
-        good.append(m)
+	if m.distance < 0.75 * n.distance:
+		good.append(m)
 
 if len(good) > MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
+	src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
+	dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
 
-    #################################################
-    M = # AQUI ENTRA A SUA FUNÇÃO DE HOMOGRAFIA!!!!
-    #################################################
+	#################################################
+	M = # AQUI ENTRA A SUA FUNÇÃO DE HOMOGRAFIA!!!!
+	#################################################
 
-    img4 = cv.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
+	img4 = cv.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
 
 else:
-    print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
-    matchesMask = None
+	print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+	matchesMask = None
 
 draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   flags = 2)
+				   singlePointColor = None,
+				   flags = 2)
 img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
 
 fig, axs = plt.subplots(2, 2, figsize=(30, 15))
--- T2/T2_Homography_MauricioBPimenta_GPT.py
+++ T2/T2_Homography_MauricioBPimenta_GPT.py
@@ -16,38 +16,49 @@ import cv2 as cv
 # Saída: norm_points (pontos normalizados)
 #        T (matriz de normalização)
 def normalize_points(points):
-    if len(points.shape) == 1:
-        points = points[:, np.newaxis]
+	if len(points.shape) == 1:
+		points = points[:, np.newaxis]
 
-    centroid = np.mean(points, axis=1)
-    mean_distance = np.mean(np.sqrt(np.sum((points - centroid[:, None])**2, axis=0)))
+	# Calculate centroid
+	centroid = np.mean(points, axis=0)
+	# Calculate the average distance of the points having the centroid as origin
+	mean_distance = np.mean(np.sqrt(np.sum((points - centroid[:, None])**2, axis=0)))
 
-    if mean_distance == 0:
-        return points, np.eye(3)
+	if mean_distance == 0:
+		return points, np.eye(3)
 
-    T = np.array([[np.sqrt(2)/mean_distance, 0, -np.sqrt(2)/mean_distance * centroid[0]],
-                  [0, np.sqrt(2)/mean_distance, -np.sqrt(2)/mean_distance * centroid[1]],
-                  [0, 0, 1]])
+	# Define the scale to have the average distance as sqrt(2)
+	
+	# Define the normalization matrix (similar transformation)
+	
+	# Normalize points
+	
 
-    norm_points = np.dot(T[:2, :2], points) + T[:2, 2][:, None]
 
 
-    return norm_points, T
+	T = np.array([[np.sqrt(2)/mean_distance, 0, -np.sqrt(2)/mean_distance * centroid[0]],
+				  [0, np.sqrt(2)/mean_distance, -np.sqrt(2)/mean_distance * centroid[1]],
+				  [0, 0, 1]])
+
+	norm_points = np.dot(T[:2, :2], points) + T[:2, 2][:, None]
+
+
+	return norm_points, T
 
 # Função para montar a matriz A do sistema de equações do DLT
 # Entrada: pts1, pts2 (pontos "pts1" da primeira imagem e pontos "pts2" da segunda imagem que atendem a pts2=H.pts1)
 # Saída: A (matriz com as duas ou três linhas resultantes da relação pts2 x H.pts1 = 0)
 def compute_A(pts1, pts2):
 
-    A = np.zeros((2 * len(pts1), 9))
+	A = np.zeros((2 * len(pts1), 9))
 
-    for i in range(len(pts1[0])):
-        x, y = pts1[:, i]
-        xp, yp = pts2[:, i]
-        A[2 * i, :] = [-x, -y, 0, 0, 0, 0, x * xp, y * xp, xp]
-        A[2 * i + 1, :] = [0, 0, 0, -x, -y, 0, x * yp, y * yp, yp]
+	for i in range(len(pts1[0])):
+		x, y = pts1[:, i]
+		xp, yp = pts2[:, i]
+		A[2 * i, :] = [-x, -y, 0, 0, 0, 0, x * xp, y * xp, xp]
+		A[2 * i + 1, :] = [0, 0, 0, -x, -y, 0, x * yp, y * yp, yp]
 
-    return A
+	return A
 
 
 # Função do DLT Normalizado
@@ -55,26 +66,26 @@ def compute_A(pts1, pts2):
 # Saída: H (matriz de homografia estimada)
 def compute_normalized_dlt(pts1, pts2):
 
-    # Normaliza pontos
-    norm_pts1, T1 = normalize_points(pts1)
-    norm_pts2, T2 = normalize_points(pts2)
+	# Normaliza pontos
+	norm_pts1, T1 = normalize_points(pts1)
+	norm_pts2, T2 = normalize_points(pts2)
 
-    # Constrói o sistema de equações empilhando a matrix A de cada par de pontos correspondentes normalizados
-    A = compute_A(norm_pts1, norm_pts2)
+	# Constrói o sistema de equações empilhando a matrix A de cada par de pontos correspondentes normalizados
+	A = compute_A(norm_pts1, norm_pts2)
 
-    # Calcula o SVD da matriz A_empilhada e estima a homografia H_normalizada
-    try:
-        _, _, V = np.linalg.svd(A)
-        H_normalized = V[-1, :].reshape(3, 3)
+	# Calcula o SVD da matriz A_empilhada e estima a homografia H_normalizada
+	try:
+		_, _, V = np.linalg.svd(A)
+		H_normalized = V[-1, :].reshape(3, 3)
 
-        # Denormaliza H_normalizada e obtém H
-        H = np.dot(np.linalg.inv(T2), np.dot(H_normalized, T1))
+		# Denormaliza H_normalizada e obtém H
+		H = np.dot(np.linalg.inv(T2), np.dot(H_normalized, T1))
 
-        return H
+		return H
 
-    except np.linalg.LinAlgError:
-        print("A matriz de homografia não é invertível.")
-        return np.eye(3)
+	except np.linalg.LinAlgError:
+		print("A matriz de homografia não é invertível.")
+		return np.eye(3)
 
 
 # Função do RANSAC
@@ -92,50 +103,53 @@ def compute_normalized_dlt(pts1, pts2):
 
 def RANSAC(pts1, pts2, dis_threshold, N, Ninl):
 
-    # Define outros parâmetros como número de amostras do modelo, probabilidades da equação de N, etc
-    max_inliers = 0
-    best_H = None
-    best_pts1_in = None
-    best_pts2_in = None
+	# Define outros parâmetros como número de amostras do modelo, probabilidades da equação de N, etc
+	max_inliers = 0
+	best_H = None
+	best_pts1_in = None
+	best_pts2_in = None
 
-    pts1 = np.transpose(pts1)
-    pts2 = np.transpose(pts2)
+	numPoints = len(pts1); print('\n\n' , numPoints , '\n\n')
+	minPoints = 4
 
-    # Processo Iterativo
-    for _ in range(int(N)):
-        # Enquanto não atende a critério de parada
-        if len(pts1[0]) >= 4:
+	#pts1 = np.transpose(pts1)[0]
+	#pts2 = np.transpose(pts2)[0]
 
-            # Sorteia aleatoriamente "s" amostras do conjunto de pares de pontos pts1 e pts2
-            indices = np.random.choice(len(pts1[0]), size=4, replace=False)
-            sampled_pts1 = pts1[:, indices]
-            sampled_pts2 = pts2[:, indices]
+	# Processo Iterativo
+	for _ in range(int(N)):
+		# Enquanto não atende a critério de parada
+		if len(pts1[0]) >= 4:
 
-            # Usa as amostras para estimar uma homografia usando o DTL Normalizado
-            H = compute_normalized_dlt(sampled_pts1, sampled_pts2)
+			# Sorteia aleatoriamente "s" amostras do conjunto de pares de pontos pts1 e pts2
+			indices = np.random.choice(numPoints, size=minPoints, replace=False)
+			sampled_pts1 = pts1[indices]
+			sampled_pts2 = pts2[indices]
 
-            # Testa essa homografia com os demais pares de pontos usando o dis_threshold e contabiliza
-            # o número de supostos inliers obtidos com o modelo estimado
-            distances = np.sqrt(np.sum((pts2 - np.dot(H, pts1))**2, axis=0))
-            inliers = np.sum(distances < dis_threshold)
+			# Usa as amostras para estimar uma homografia usando o DTL Normalizado
+			H = compute_normalized_dlt(sampled_pts1, sampled_pts2)
 
-            # Se o número de inliers é o maior obtido até o momento, guarda esse conjunto além das "s" amostras utilizadas.
-            # Atualiza também o número N de iterações necessárias
-            if inliers > max_inliers:
-                max_inliers = inliers
-                best_H = H
-                best_pts1_in = pts1[:, distances < dis_threshold]
-                best_pts2_in = pts2[:, distances < dis_threshold]
+			# Testa essa homografia com os demais pares de pontos usando o dis_threshold e contabiliza
+			# o número de supostos inliers obtidos com o modelo estimado
+			distances = np.sqrt(np.sum((pts2 - np.dot(H, pts1))**2, axis=0))
+			inliers = np.sum(distances < dis_threshold)
 
-                p_inlier = inliers / len(pts1[0])
-                epsilon = 1e-10
-                N = min(N, np.log(epsilon) / np.log(1 - p_inlier**4))
+			# Se o número de inliers é o maior obtido até o momento, guarda esse conjunto além das "s" amostras utilizadas.
+			# Atualiza também o número N de iterações necessárias
+			if inliers > max_inliers:
+				max_inliers = inliers
+				best_H = H
+				best_pts1_in = pts1[distances < dis_threshold]
+				best_pts2_in = pts2[distances < dis_threshold]
 
-    # Terminado o processo iterativo
-    # Estima a homografia final H usando todos os inliers selecionados.
-    H_final = compute_normalized_dlt(best_pts1_in, best_pts2_in)
+				p_inlier = inliers / len(pts1[0])
+				epsilon = 1e-10
+				N = min(N, np.log(epsilon) / np.log(1 - p_inlier**4))
 
-    return H_final, best_pts1_in, best_pts2_in
+	# Terminado o processo iterativo
+	# Estima a homografia final H usando todos os inliers selecionados.
+	H_final = compute_normalized_dlt(best_pts1_in, best_pts2_in)
+
+	return H_final, best_pts1_in, best_pts2_in
 
 
 ########################################################################################################################
@@ -148,19 +162,19 @@ img2 = cv.imread('./imagens/comicsStarWars02.jpg', 0)        # trainImage
 
 # Inicialização do SIFT
 try:
-    # Tentar usar a versão mais recente
-    sift = cv.SIFT_create()
+	# Tentar usar a versão mais recente
+	sift = cv.SIFT_create()
 except AttributeError:
-    # Se falhar, tentar a versão mais antiga
-    sift = cv.xfeatures2d.SIFT_create()
+	# Se falhar, tentar a versão mais antiga
+	sift = cv.xfeatures2d.SIFT_create()
 
 kp1, des1 = sift.detectAndCompute(img1, None)
 kp2, des2 = sift.detectAndCompute(img2, None)
 
 # Verifica se há pontos de correspondência suficientes
 if len(kp1) < MIN_MATCH_COUNT or len(kp2) < MIN_MATCH_COUNT:
-    print("Not enough matches are found - {}/{}".format(min(len(kp1), len(kp2)), MIN_MATCH_COUNT))
-    exit()
+	print("Not enough matches are found - {}/{}".format(min(len(kp1), len(kp2)), MIN_MATCH_COUNT))
+	exit()
 
 # FLANN
 FLANN_INDEX_KDTREE = 1
@@ -171,29 +185,29 @@ matches = flann.knnMatch(des1, des2, k=2)
 
 good = []
 for m, n in matches:
-    if m.distance < 0.75 * n.distance:
-        good.append(m)
+	if m.distance < 0.75 * n.distance:
+		good.append(m)
 
 if len(good) > MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
+	src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
+	dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
 
-    # Aplicação da função RANSAC
-    #################################################
-    M, pts1_in, pts2_in = RANSAC(src_pts, dst_pts, dis_threshold=2, N=1000, Ninl=MIN_MATCH_COUNT)   # AQUI ENTRA A SUA FUNÇÃO DE HOMOGRAFIA!!!!
-    #################################################
+	# Aplicação da função RANSAC
+	#################################################
+	M, pts1_in, pts2_in = RANSAC(src_pts, dst_pts, dis_threshold=2, N=1000, Ninl=MIN_MATCH_COUNT)   # AQUI ENTRA A SUA FUNÇÃO DE HOMOGRAFIA!!!!
+	#################################################
 
-    # Aplicação da homografia na imagem de entrada
-    img4 = cv.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
+	# Aplicação da homografia na imagem de entrada
+	img4 = cv.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
 
 else:
-    print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
-    matchesMask = None
+	print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+	matchesMask = None
 
 # Desenho dos matches e visualização das imagens
 draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   flags = 2)
+				   singlePointColor = None,
+				   flags = 2)
 img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
 
 fig, axs = plt.subplots(2, 2, figsize=(30, 15))
